2b9e20667a2c7c70d81433eb765699aa3d7f4ba4
Duynguyen <duynguyen0444@gmail.com>
2025-08-23 16:53:26 +0700

    mod.structure project files

---
+++ src/components/AppBar/AppBar.jsx
@@ -0,0 +1,134 @@
+import React, { useState } from 'react'
+import AppsIcon from '@mui/icons-material/Apps'
+import HelpOutlineIcon from '@mui/icons-material/HelpOutline'
+import LibraryAddIcon from '@mui/icons-material/LibraryAdd'
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone'
+import Badge from '@mui/material/Badge'
+import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
+import SvgIcon from '@mui/material/SvgIcon'
+import TextField from '@mui/material/TextField'
+import Tooltip from '@mui/material/Tooltip'
+import Typography from '@mui/material/Typography'
+import { ReactComponent as TrelloIcon } from '~/assets/trello.svg'
+import Workspace from '~/components/AppBar/Menus/Workspace'
+import ModeSelect from '~/components/ModeSelect/ModeSelect'
+import Profiles from './Menus/Profiles'
+import Recent from './Menus/Recent'
+import Starred from './Menus/Starred'
+import Template from './Menus/Template'
+import CloseIcon from '@mui/icons-material/Close';
+import { InputAdornment } from '@mui/material'
+import { Search as SearchIcon } from '@mui/icons-material'
+
+function AppBar() {
+  const [searchValue, setSearchValue] = useState('')
+
+  return (
+    <Box sx={{
+      width: '100%',
+      height: (theme) => theme.trello.appBarHeight,
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'space-between',
+      gap: 2,
+      paddingX: 2,
+      overflowX: 'auto',
+      bgcolor: (theme) => theme.palette.mode === 'dark' ? '#2c3e50' : '#1565c0'
+    }}>
+      <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+        <AppsIcon sx={{ color: 'white' }} />
+        <Box sx={{ display: 'flex', alignItems: 'center', gap: 0.5 }}>
+          <SvgIcon component={TrelloIcon} fontSize="small" inheritViewBox sx={{ color: 'white' }} />
+          <Typography variant='span' sx={{
+            fontSize: '1.2rem',
+            fontWeight: 'bold',
+            color: 'white'
+          }}>
+            Trello
+          </Typography>
+        </Box>
+
+        <Box sx={{
+          display: {
+            xs: 'none',
+            md: 'flex'
+          },
+          gap: 1
+        }}>
+          <Workspace />
+          <Recent />
+          <Starred />
+          <Template />
+          <Button
+            startIcon={<LibraryAddIcon />}
+            variant="outlined"
+            sx={{
+              color: 'white',
+              border: 'none',
+              '&:hover': {
+                border: 'none'
+              }
+            }}
+          >
+            Create
+          </Button>
+        </Box>
+
+      </Box>
+
+      <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+        <TextField
+          id="outlined-search"
+          label="Search..."
+          type="text"
+          size="small"
+          value={searchValue}
+          onChange={(e) => setSearchValue(e.target.value)}
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <SearchIcon sx={{ color: 'white' }} />
+              </InputAdornment>
+            ),
+            endAdornment: (
+              <CloseIcon
+                fontSize='small'
+                onClick={() => setSearchValue('')}
+                sx={{
+                  color: searchValue ? 'white' : 'transparent',
+                  cursor: searchValue ? 'pointer' : 'default'
+                }}
+              />
+            )
+          }}
+          sx={{
+            minWidth: '120px',
+            maxWidth: '170px',
+            '& label': { color: 'white' },
+            '& input': { color: 'white' },
+            '& label.Mui-focused': { color: 'white' },
+            '& .MuiOutlinedInput-root': {
+              '& fieldset': { borderColor: 'white' },
+              '& :hover fieldset': { borderColor: 'white' },
+              '&.Mui-focused fieldset': { borderColor: 'white' }
+            }
+          }}
+        />
+        <ModeSelect />
+        <Tooltip title="Notifications">
+          <Badge color="warning" variant="dot" sx={{ cursor: 'pointer' }}>
+            <NotificationsNoneIcon sx={{ color: 'white' }} />
+          </Badge>
+        </Tooltip>
+
+        <Tooltip title="Help">
+          <HelpOutlineIcon sx={{ color: 'white', cursor: 'pointer' }} />
+        </Tooltip>
+        <Profiles />
+      </Box>
+    </Box>
+  )
+}
+
+export default AppBar
---
+++ src/pages/Boards/BoardBar/BoardBar.jsx
@@ -0,0 +1,119 @@
+
+import { AddToDrive, Bolt, FilterList } from '@mui/icons-material'
+import DashboardIcon from '@mui/icons-material/Dashboard'
+import VpnLockIcon from '@mui/icons-material/VpnLock'
+import Avatar from '@mui/material/Avatar'
+import AvatarGroup from '@mui/material/AvatarGroup'
+import Box from '@mui/material/Box'
+import Chip from '@mui/material/Chip'
+import Tooltip from '@mui/material/Tooltip'
+import Button from '@mui/material/Button'
+import PersonAddIcon from '@mui/icons-material/PersonAdd'
+
+const MENU_STYLES = {
+  color: 'white',
+  backgroundColor: 'transparent',
+  border: 'none',
+  paddingX: '5px',
+  borderRadius: '4px',
+  '.MuiSvgIcon-root': {
+    color: 'white'
+  },
+  '&:hover': {
+    bgcolor: 'primary.50'
+  }
+}
+
+function BoardBar() {
+  return (
+    <Box sx={{
+      width: '100%',
+      height: (theme) => theme.trello.boardBarHeight,
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'space-between',
+      gap: 2,
+      paddingX: 2,
+      overflowX: 'auto',
+      bgcolor: (theme) => theme.palette.mode === 'dark' ? '#34495e' : '#1976d2'
+    }}>
+      <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+        <Chip
+          icon={<DashboardIcon />}
+          label="MernStack Board"
+          clickable
+          sx={MENU_STYLES}
+        />
+
+        <Chip
+          icon={<VpnLockIcon />}
+          label="Public/Private Workspace"
+          clickable
+          sx={MENU_STYLES}
+        />
+        <Chip
+          icon={<AddToDrive />}
+          label="Add to Google Drive"
+          clickable
+          sx={MENU_STYLES}
+        />
+        <Chip
+          icon={<Bolt />}
+          label="Automation"
+          clickable
+          sx={MENU_STYLES}
+        />
+
+        <Chip
+          icon={<FilterList />}
+          label="Filters"
+          clickable
+          sx={MENU_STYLES}
+        />
+      </Box>
+
+      <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+        <Button
+          startIcon={<PersonAddIcon />}
+          variant="outlined"
+          sx={{
+            color: 'white',
+            borderColor: 'white',
+            '&:hover': { borderColor: 'white' }
+          }}
+        >
+          Invite
+        </Button>
+
+        <AvatarGroup
+          max={7}
+          sx={{
+            gap: '10px',
+            '& .MuiAvatar-root': {
+              width: 34,
+              height: 34,
+              fontSize: '16px',
+              border: 'none',
+              color: 'white',
+              cursor: 'pointer',
+              '&:first-of-type': { bg: '#a4b0de' }
+            }
+          }}
+        >
+          <Tooltip title="Remy Sharp" >
+            <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+          </Tooltip>
+          <Tooltip title="Remy Sharp" >
+            <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+          </Tooltip> <Tooltip title="Remy Sharp" >
+            <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+          </Tooltip> <Tooltip title="Remy Sharp" >
+            <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+          </Tooltip>
+        </AvatarGroup>
+      </Box>
+    </Box>
+  )
+}
+
+export default BoardBar
